6b7b424ac6301747e5478bec9faad4dde856a215
Abdirazakf <farahabdirazak13@gmail.com>
2026-01-08 00:53:45 -0500

    Added login and sign up pages to route as guest routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import { useEffect } from 'react'
 import {useAuthStore} from './states/useAuthStore'
 import {Routes, Route} from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import GuestRoute from './components/GuestRoute'
 import Homepage from './pages/Homepage'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
 export default function App(){
   const checkAuth = useAuthStore((state) => state.checkAuth)
@@ -15,6 +18,21 @@ export default function App(){
     <div className="min-h-screen bg-background text-zinc-400 font-sans antialiased h-screen w-screen overflow-hidden selection:bg-white/10">
       <Routes>
         <Route path='/' element={<Homepage/>} />
+
+        <Route path='/sign-up'
+        element={
+          <GuestRoute>
+            <SignUp />
+          </GuestRoute>
+        } />
+
+        <Route path='/login'
+        element={
+          <GuestRoute>
+            <Login />
+          </GuestRoute>
+        } />
+
       </Routes>
 
       <Toaster />
