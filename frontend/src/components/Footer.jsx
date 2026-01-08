17cdf7eb0b660f30151c389239537fda1b13d9cc
Abdirazakf <farahabdirazak13@gmail.com>
2026-01-07 23:01:01 -0500

    Completed footer and navbar components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return(
+        <footer className="border-t border-zinc-800 bg-background py-8 relative z-10">
+            <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-center items-center gap-4 text-xs text-zinc-600">
+                    <a href="https://github.com/Abdirazakf" target="blank" rel="noopener noreferrer" >
+                        <span>Created by Abdirazak Farah</span>
+                    </a>
+            </div>
+        </footer>
+    )
+}
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {Box} from 'lucide-react'
+import { Box } from 'lucide-react'
 import { Link } from 'react-router'
 
 export default function Navbar(){
@@ -7,23 +7,23 @@ export default function Navbar(){
             <div className="max-w-7xl mx-auto px-6 h-14 flex items-center justify-between">
                 <div className="flex items-center gap-2 text-zinc-100 font-semibold tracking-tight">
                     <div className="w-6 h-6 bg-zinc-100 text-black flex items-center justify-center rounded-[4px]">
-                        <span className="iconify">
-                            <Box />
-                        </span>
+                        <Box size={14} />
                     </div>
                     <span>UR FILES</span>
                 </div>
                 <div className="flex items-center gap-6">
-                    <Link to={'/login'}>
-                        <button className="cursor-pointer text-sm font-medium text-zinc-300 hover:text-white">
-                            Login
-                        </button>
-                    </Link>
-                    <Link to={'/sign-up'}>
-                        <button class="cursor-pointer text-sm font-medium bg-zinc-100 hover:bg-white text-black px-3 py-1.5 rounded-[4px] transition-colors">
-                            Sign Up
-                        </button>
-                    </Link>
+                    <div className="flex items-center gap-3">
+                        <Link to="/login">
+                            <button className="cursor-pointer text-sm font-medium text-zinc-300 hover:text-white transition-colors">
+                                Log in
+                            </button>
+                        </Link>
+                        <Link to="/sign-up">
+                            <button className="cursor-pointer text-sm font-medium bg-zinc-100 hover:bg-white text-black px-3 py-1.5 rounded-[4px] transition-colors">
+                                Sign Up
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </nav>
