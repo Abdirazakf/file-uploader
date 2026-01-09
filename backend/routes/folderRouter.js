a6ef4d05ef3f8aa7e997ccde4ed49032142ae982
Abdirazakf <farahabdirazak13@gmail.com>
2026-01-09 03:48:08 -0500

    Completed the folder route

---
+++ backend/routes/folderRouter.js
@@ -0,0 +1,19 @@
+const {Router} = require('express')
+const folderRouter = Router()
+const {
+        getUserFolders,
+        getFolderByID,
+        createFolderPost,
+        updateFolderPut,
+        deleteFolder} = require('../controllers/folderController')
+
+folderRouter.get('/', getUserFolders)
+folderRouter.get('/:id', getFolderByID)
+
+folderRouter.post('/', createFolderPost)
+
+folderRouter.put('/:id', updateFolderPut)
+
+folderRouter.delete('/:id', deleteFolder)
+
+module.exports = folderRouter
