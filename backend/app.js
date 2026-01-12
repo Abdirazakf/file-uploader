b22b9b7182fbd5987002c5372857e1f917d0dfe1
Abdirazakf <farahabdirazak13@gmail.com>
2026-01-11 21:03:35 -0500

    Added file router to app and removed placholder upload controller and router

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,8 @@ const path = require('path')
 const signupRouter = require('./routes/signupRouter.js')
 const loginRouter = require('./routes/loginRouter.js')
 const authRouter = require('./routes/authRouter.js')
-const uploadRouter = require('./routes/uploadRouter.js')
 const folderRouter = require('./routes/folderRouter.js')
+const fileRouter = require('./routes/fileRouter.js')
 
 const { isAuthenticated } = require('./middleware/authMiddleware.js')
 
@@ -61,8 +61,8 @@ app.use(passport.session())
 app.use('/api/sign-up', signupRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/auth', authRouter)
-app.use('/api/upload', uploadRouter)
 app.use('/api/folders', isAuthenticated, folderRouter)
+app.use('/api/files', isAuthenticated, fileRouter)
 
 if (process.env.NODE_ENV === 'prod'){
     app.use(express.static(path.join(__dirname, '../frontend/dist')))
