3a23411b10fbd3af56746174691646e161ee45af
Abdirazakf <farahabdirazak13@gmail.com>
2026-01-12 14:30:26 -0500

    Created auth route to protect folder view routes

---
+++ frontend/src/components/AuthRoute.jsx
@@ -0,0 +1,21 @@
+import { ThreeDot } from "react-loading-indicators";
+import { useAuthStore } from "../states/useAuthStore";
+import { Navigate } from "react-router";
+
+export default function GuestRoute({children}){
+    const {auth, loading} = useAuthStore()
+
+    if (loading){
+        return(
+            <div className="flex items-center justify-center min-h-screen">
+                <ThreeDot color={"white"} size="medium" />
+            </div>
+        )
+    }
+
+    if (!auth){
+        return <Navigate to={'/login'} replace />
+    }
+
+    return children
+}
